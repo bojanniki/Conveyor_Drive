7dc11c4963020e7cb768cf52b7dc19f8d0e8333f
Bojan <bojan.niki@gmail.com>
2026-01-27 21:23:33 +0100

    - Implementirana formula za površinski pritisak pera u Jupyter proračun. - Izračunat minimalni promjer vratila i sigurnost na uvijanje (S_vratilo). - Definirane tolerancije širine pera (h9) i visine (h11) u 3D modelu.

--- 02_CAD_Modeli/Assembly/sklop_prijenosnika.asm
+++ 02_CAD_Modeli/Assembly/sklop_prijenosnika.asm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e175273e4ac36ebf50b07781014054f3caaf986592567cf4f88bec462db6d4ba
-size 290816
+oid sha256:a84600ee297173c5b871b7e11fce76e92db6917e9385905302bab21b98e86254
+size 294912
