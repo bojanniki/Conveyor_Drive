2d0208a36080c33cacb3ceb709590cc30c0c4ce8
Bojan <bojan.niki@gmail.com>
2026-04-03 22:18:19 +0200

    Definirano kućište. Započeta izrada sklopa

---
+++ 02_CAD_Modeli/Assembly/sklop_kuciste_prijenosnik.asm
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:50cbdd0b687130db8dc1bd476911b084a03610936b35ed6148904315c486d606
+size 704512
--- 02_CAD_Modeli/Assembly/sklop_prijenosnika.asm
+++ 02_CAD_Modeli/Assembly/sklop_prijenosnika.asm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9c8edfadc174f2bc7f210bd6caec3f663607de48e8438fc76da145dda4caf7e4
-size 393216
+oid sha256:ffe61154fdbb690940c33e01f5d45258fd009388a8f126231880154a49ad6e15
+size 679936
