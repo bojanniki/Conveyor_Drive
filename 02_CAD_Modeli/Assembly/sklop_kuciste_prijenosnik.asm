0b7e107ee1e7cfd08fb8cf2613ccf9225f93f9fa
Bojan <bojan.niki@gmail.com>
2026-04-06 13:34:10 +0200

    Modeliranje kućišta - dodane prirubnice i ribbs na kućište

--- 02_CAD_Modeli/Assembly/sklop_kuciste_prijenosnik.asm
+++ 02_CAD_Modeli/Assembly/sklop_kuciste_prijenosnik.asm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20fb09e898bac09853cc3b9698be0d7ad50ca831c9a85ef61552045cc275abd9
+oid sha256:2c7afc0e586b7f60a808aa9ddda008b173cf62d7008d412571667510d6d9b7a6
 size 815104
