c7191e2c18feb0618d1c1ac092bd75666d523606
Bojan <bojan.niki@gmail.com>
2026-04-19 21:48:50 +0200

    Dodan model poklopca i vijci za poklopac. Dodan provrt za ispust masti

--- 02_CAD_Modeli/Assembly/sklop_kuciste_prijenosnik.asm
+++ 02_CAD_Modeli/Assembly/sklop_kuciste_prijenosnik.asm
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb93d540ac6ce42e93b27e341ad4ac354a31e820b7beffdb5d9394b8a0190e1b
-size 1167360
+oid sha256:a1a0a5a9eac79cda7899e3bcb79efe0d56d0a54a6ff0c167222fba62999dc55d
+size 1220608
